68ce6479f9e3fea7cba2e9041c323d8ab15726f5
EageRAssassin <rt389@cornell.edu>
2018-03-21 21:46:05 -0400

    state function added

--- state.mli
+++ state.mli
@@ -1,11 +1,12 @@
 open billiard
 open player
 open foul
-(*the type state contains the following fields:
+
+(*The type state contains the following fields:
   [on_board] : the list of billiard that is on the board
   [player] : the list of players in the game
   [is_pot] : the list of billard that has been pot in the pocket
-  [foul] :
+  [foul] : type of foul that the current user triggered
   [is_playing] : the player that is playing the game in the state
   [ball_moving] : true if one of the ball is moving, false if all not moving 
 *)
@@ -17,3 +18,24 @@ type state = {
   is_playing : player;
   ball_moving : bool;
 }
+
+(* [change_state st] will change the inner attributes of [st] into 
+ * the next state
+ *)
+val change_state : state -> state
+
+(* [kinetic_transfer direction force] will  
+ *
+ *)
+val kinetic_transfer : ( int * int ) -> state -> state
+
+(* [next_turn st] will  
+ *
+ *)
+val next_turn : state -> state 
+
+(* [ai_evaluate_next_move st] will use our AI to analyze the current 
+ * state to produce the optimal direction and speed to play against 
+ * the player 
+ *)
+val ai_evaluate_next_move : state -> ( int * int )
