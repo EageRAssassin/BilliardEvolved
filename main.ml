b2cdaf2a99201fe4dc5ae8b9ee1ac73094061bdd
EageRAssassin <rt389@cornell.edu>
2018-04-24 22:34:03 -0400

    main loaded

---
+++ main.ml
@@ -0,0 +1,51 @@
+open Types
+
+(* js_of_ocaml helper declarations *)
+module Html = Dom_html
+let js = Js.string
+let document = Html.document
+
+(************************ DOM HELPERS ************************)
+
+(* [fail] is a failure/exception handler *)
+let fail = fun _ -> assert false
+
+(* [get_element_by_id id] gets a DOM element by its id *)
+let get_element_by_id id =
+  Js.Opt.get (Html.document##getElementById (js id)) fail
+
+(* [append_text e s] appends string s to element e *)
+let append_text e s = Dom.appendChild e (document##createTextNode (js s))
+
+(************************ START GAME ************************)
+
+(* [main ()] is the main method of the entire game. *)
+let main () =
+  let gui = get_element_by_id "gui" in
+  gui##style##cssText <- js "font-family:Triforce";
+  let h1 = Html.createH1 document in
+  let p = Html.createP document in
+  (* let audio = Html.createAudio document in *)
+  (* audio##src <- js "zolda.mp3"; *)
+  (* audio##play (); *)
+  append_text h1 "The Legend of Zolda";
+  gui##style##cssText <- js "font-family:Triforce font-size:10px";
+  append_text p "You are Lonk. Save Zolda!";
+  Dom.appendChild gui h1;
+  Dom.appendChild gui p;
+  let canvas = Html.createCanvas document in
+  canvas##width <- int_of_float Gui.canvas_width;
+  canvas##height <- int_of_float Gui.canvas_height;
+  Dom.appendChild gui canvas;
+  let context = canvas##getContext (Html._2d_) in
+  (* add event listeners *)
+  let _ = Html.addEventListener
+      document Html.Event.keydown (Html.handler Game.keydown)
+      Js._true in
+  let _ = Html.addEventListener
+      document Html.Event.keydown (Html.handler Game.keyup)
+      Js._true in
+  Game.game_loop context false
+
+(* start the game *)
+let _ = main ()
