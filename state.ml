17c3c7000529dc2e11f683cdbbd6ec39142e3b5d
WendyHuang <bh486@cornell.edu>
2018-04-26 01:00:26 -0400

    update

--- state.ml
+++ state.ml
@@ -31,19 +31,65 @@ type state = {
 
 let radius = 1
 
-(* [collide b1 b2]Collides two balls and changes their velocities accordingly 
-   requires : b1 and b2 are valid billiard records 
+(* [collide b1 b2]Collides two balls and changes their velocities accordingly
+   requires : b1 and b2 are valid billiard records
    returns  : the tuple of the 2 billards with speed changes *)
-let collide (b1 : billiard) (b2 : billiard) : (billiard * billiard)= 
-  let x1 = fst (b1.position) + radius in 
-  let x2 = fst (b2.position) + radius in 
-  let y1 = snd (b1.position) + radius in 
-  let y2 = snd (b2.position) + radius in    
-  let dxR = x2 - x1 in 
-  let dyR = y2 - y1 in 
+let collide (b1 : billiard) (b2 : billiard) : (billiard * billiard)=
+  let x1 = fst (b1.position) + radius ref in
+  let x2 = fst (b2.position) + radius ref in
+  let y1 = snd (b1.position) + radius ref in
+  let y2 = snd (b2.position) + radius ref in
+  let dxR = x2 - x1 in
+  let dyR = y2 - y1 in
   (* gets the ACTUAL distance between the two balls *)
-  let distance = Math.sqrt(DxR * DxR + DyR * DyR); 
+  let distance = Math.sqrt(DxR * DxR + DyR * DyR) in
 
+  (* RadSum adds radii, this gives the ideal distance *)
+  let dx = 2 *. dxR /. distance in
+  let dy = 2 *. dyR /. distance in
+
+  (*   so that things dont look screwy when we have a fast tiny ball collide with
+a slow big one we want it to be the small ball that will have its position adjusted *)
+
+
+  if b1.mass < b2.mass then
+    x1 := ( !x1 - (dx - dxR));
+    y1 := ( !x1 - (dy - dyR));
+    fst b1.position <- !x1 - radius;
+    snd b1.position <- !y1 - radius;
+    else
+    x2 := (!x2 + (dx - dxR));
+    y2 := (!y2 + (dy - dyR));
+    fst b2.position <- !x2 - radius;
+    snd b2.position <- !y2 - radius;
+
+(* Find the x and y distances from the centers of each ball to the collision point *)
+    let dx1 = (1/2 ) * (!x2 - !x1) in
+    let dx2 = (1/2 ) * (!x2 - !x1) in
+
+    let dy1 = (1/2 ) * (!y2 - !y1) in
+    let dy2 = (1/2 ) * (!y2 - !y1) in
+
+  (* calculate the components of velocity of each ball headed towards the collision point and perpendicular to it *)
+   let vs1 = straight_Velocity(fst b1.velocity, snd b1.velocity, dx1, dy1, radius);
+   let vp1 = perpendicular_Velocity(fst b1.velocity, snd b1.velocity, dx1, dy1, radius);
+
+   let vs2 = straight_Velocity(fst b2.velocity, snd b2.velocity, dx2, dy2, radius) in
+   let vp2 = perpendicular_Velocity(fst b2.velocity, snd b2.velocity, dx2, dy2, radius) in
+
+  (* use the formulas in the method to find new straight velocities for each ball *)
+   let newVs1 = collision_Velocity(vs1, vs2, b1.mass, b2.mass) in
+   let newVs2 = collision_Velocity(vs2, vs1, b2.mass, b1.mass) in
+
+  (* now we get new X and Y velocities for each, using the new straight velocity and the unaffected perpendicular velocity component
+  *)
+   let vxx_new = x_Velocity(newVs1, vp1, dx1, dy1, radius) in
+   let vxy_new = y_Velocity(newVs1, vp1, dx1, dy1, radius) in
+   b1.velocity <- (vxx_new,vxy_new);
+
+   let vyx_new = x_Velocity(newVs2, vp2, dx2, dy2, radius) in
+   let vyy_new = y_Velocity(newVs2, vp2, dx2, dy2, radius) in
+   b2.velocity <- (vxx_new,vxy_new)
 
 
 (* [change_state st] will change the attributes of fields in [st] and
